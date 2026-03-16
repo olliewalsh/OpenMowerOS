754f13be6431f2f28b7867c2b9f62ea4e6b219b7
Oliver Walsh <owalsh@redhat.com>
2026-03-16 18:31:27 +0000

    add latest upstream slattach

--- stage-openmower/10-pi-serial/00-run-chroot.sh
+++ stage-openmower/10-pi-serial/00-run-chroot.sh
@@ -1,5 +1,25 @@
 #!/bin/bash -ex
 
+# Build and install the latest upstream slattach from net-tools.
+NET_TOOLS_REPO="https://github.com/ecki/net-tools.git"
+NET_TOOLS_COMMIT="701161795e87a3b475afd7e3eb27885332cd90cb"
+BUILD_DIR="$(mktemp -d)"
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y --no-install-recommends build-essential gettext git
+
+git clone "$NET_TOOLS_REPO" "$BUILD_DIR/net-tools"
+git -C "$BUILD_DIR/net-tools" checkout "$NET_TOOLS_COMMIT"
+
+cd "$BUILD_DIR/net-tools"
+printf '\n%.0s' $(seq 1 64) | make config
+make slattach
+install -m 0755 slattach /usr/local/sbin/slattach
+
+cd /
+rm -rf "$BUILD_DIR"
+
 # Remove any serial console entries while preserving the rest of the line
 sed -i -E 's/(^| )console=(serial0|ttyAMA0|ttyS0)(,[0-9]+)?//g' /boot/firmware/cmdline.txt
 # Collapse multiple spaces and trim
